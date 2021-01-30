dbc5079dd6df2e7af9e2c4f2c64a1f39d53087c8
Yasuko Kurata <yasuko@Yasukos-MacBook-Air.local>
2021-01-30 12:55:12 -0500

    initial commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Yasuko Kurata on 2021-01-18.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
